4563468e2f1f606420d2aca5c50a608c72fcb6b0
Garrett Squire <github@garrettsquire.com>
2020-03-28 11:54:20 -0700

    polish tests and add random stream route

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 // Why do I need decl_macro when using 'format' in the route macro?
 #![feature(proc_macro_hygiene, decl_macro)]
 
+use std::cmp;
+use std::io::Cursor;
 use std::net::SocketAddr;
 
+use rand::distributions::Uniform;
+use rand::Rng;
+use rocket::http::{ContentType, Status};
 use rocket::request::{self, FromRequest, Request};
+use rocket::response::content::Content;
+use rocket::response::Stream;
 use rocket::{get, routes, Outcome};
+use rocket_contrib::helmet::SpaceHelmet;
 use rocket_contrib::json::Json;
 use serde::Serialize;
 
 #[cfg(test)]
 mod tests;
+
+#[get("/stream-bytes/<n>")]
+fn stream_bytes(n: usize) -> Content<Stream<Cursor<Vec<u8>>>> {
+    const MAX_STREAM_SIZE: usize = 1024 * 1024;
+
+    let n = cmp::min(n, MAX_STREAM_SIZE);
+    let range = Uniform::from(0..u8::MAX);
+    let values: Vec<u8> = rand::thread_rng().sample_iter(&range).take(n).collect();
+    Content(ContentType::Binary, Stream::from(Cursor::new(values)))
+}
 
 #[derive(Serialize)]
 struct Ip {
@@ -28,7 +46,7 @@ struct UserAgent {
 }
 
 impl<'a, 'r> FromRequest<'a, 'r> for UserAgent {
-    type Error = std::convert::Infallible;
+    type Error = ();
 
     fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
         const USER_AGENT: &str = "User-Agent";
@@ -38,7 +56,7 @@ impl<'a, 'r> FromRequest<'a, 'r> for UserAgent {
             Some(a) => Outcome::Success(UserAgent {
                 agent: a.to_string(),
             }),
-            None => Outcome::Forward(()),
+            None => Outcome::Failure((Status::BadRequest, ())),
         }
     }
 }
@@ -50,6 +68,7 @@ fn user_agent(agent: UserAgent) -> Json<UserAgent> {
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![ip, user_agent])
+        .attach(SpaceHelmet::default())
+        .mount("/", routes![ip, stream_bytes, user_agent])
         .launch();
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,10 +1,17 @@
-use rocket::http::Status;
+use rocket::http::{Header, Status};
 use rocket::{self, local::Client, routes};
+
+fn setup() -> Client {
+    let rocket = rocket::ignite().mount(
+        "/",
+        routes![super::ip, super::stream_bytes, super::user_agent],
+    );
+    Client::new(rocket).unwrap()
+}
 
 #[test]
 fn ip() {
-    let rocket = rocket::ignite().mount("/", routes![super::ip]);
-    let client = Client::new(rocket).unwrap();
+    let client = setup();
     let mut response = client
         .get("/ip")
         .remote("127.0.0.1:8000".parse().unwrap())
@@ -15,4 +22,37 @@ fn ip() {
         response.body_string().unwrap(),
         r#"{"origin":"127.0.0.1:8000"}"#
     );
+}
+
+#[test]
+fn user_agent_header_present() {
+    let client = setup();
+    let header_names = vec!["User-Agent", "user-agent", "user-Agent", "User-agent"];
+
+    for agent in header_names {
+        let mut response = client
+            .get("/user-agent")
+            .header(Header::new(agent, "test"))
+            .dispatch();
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.body_string().unwrap(), r#"{"user-agent":"test"}"#);
+    }
+}
+
+#[test]
+fn user_agent_header_missing() {
+    let client = setup();
+    let response = client.get("/user-agent").dispatch();
+
+    assert_eq!(response.status(), Status::BadRequest);
+}
+
+#[test]
+fn stream_bytes() {
+    let client = setup();
+    let mut response = client.get("/stream-bytes/7").dispatch();
+
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.body_bytes().unwrap().len(), 7);
 }
